57b855e677ac4195f3c83ab04972864b8a2a90e8
kevuo <kevgiso@hotmail.com>
2016-03-22 17:32:12 -0600

    Revert "Revert "Add GUI to the project.""
    This reverts commit 3fd7922ee3906a0a1647cc715fed806cf8aa75a6.

---
+++ Mercury/src/mercury/Mercury.java
@@ -0,0 +1,30 @@
+package mercury;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Mercury extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Scene scene = new Scene(root, 1024, 675);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
