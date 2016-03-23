df64a991d2de753cbd88721ca94b2d942a31647f
kevuo <kevgiso@hotmail.com>
2016-03-22 19:11:26 -0600

    Add GUI files to main project.

---
+++ mercury/gui/GUI.java
@@ -0,0 +1,18 @@
+package mercury.gui;/**
+ * Created by Kevin on 22/03/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
