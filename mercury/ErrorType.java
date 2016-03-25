c9727b37876ac85996b34f4ad6573893ba745c7a
Manuel Zumbado <manu3193@gmail.com>
2016-03-24 18:16:32 -0600

    Created ErrorType class

---
+++ mercury/ErrorType.java
@@ -0,0 +1,12 @@
+package mercury;
+
+/**
+ * This class has the types of errors found in all the stages of the program
+ */
+
+public final class ErrorType {
+
+    public static final String LEXICAL_ERROR = "Lexical Error";
+    public static final String SYNTACTIC_ERROR = "Syntactic Error";
+    public static final String SEMANTIC_ERROR = "Semantic Error";
+}
