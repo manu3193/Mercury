0ba736168a5a9518bb757093a028b291b8fdce10
Manuel Zumbado <manu3193@gmail.com>
2016-03-24 18:16:12 -0600

    Added type of error

--- mercury/ErrorMessage.java
+++ mercury/ErrorMessage.java
@@ -8,11 +8,13 @@ public class ErrorMessage {
     private int line;
     private int pos;
     private String message;
+    private String type;
 
-    public ErrorMessage(int line, int pos, String message) {
+    public ErrorMessage(int line, int pos, String message, String type) {
         this.line = line;
         this.pos = pos;
         this.message = message;
+        this.type = type;
     }
 
     public int getLine() {
@@ -22,6 +24,8 @@ public class ErrorMessage {
     public int getPos() {
         return pos;
     }
+
+    public String getType(){ return type;}
 
     public String getMessage() {
         return message;
