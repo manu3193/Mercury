39da3b252a8c3dbaedd199321f83c4d8493819c5
Manuel Zumbado <manu3193@gmail.com>
2016-04-04 07:02:05 -0600

    Added base methods and class members

--- mercury/ARMProgram.java
+++ mercury/ARMProgram.java
@@ -1,10 +1,49 @@
 package mercury;
 
+import mercury.arm.instruction.LabelStatement;
+
+import java.util.List;
+
 /**
  * Created by manzumbado on 19/03/16.
  */
 public class ARMProgram {
 
-    private String filename;
-    //private
+    private String mFilename;
+    private List<ProgramStatement> mStatementList;
+    private List<LabelStatement> mSetLabelList;
+    private ErrorList mErrorList;
+
+
+    public ARMProgram(String mFilename) {
+        this.mFilename = mFilename;
+    }
+
+    public List<ProgramStatement> getStatementList() {
+        return mStatementList;
+    }
+
+    public String getFilename() {
+        return mFilename;
+    }
+
+    public void setStatementList(List<ProgramStatement> mStatementList) {
+        this.mStatementList = mStatementList;
+    }
+
+    public ErrorList getErrorList() {
+        return mErrorList;
+    }
+
+    public void setErrorList(ErrorList mErrorList) {
+        this.mErrorList = mErrorList;
+    }
+
+    public List<LabelStatement> getSetLabelList() {
+        return mSetLabelList;
+    }
+
+    public void setSetLabelList(List<LabelStatement> mSetLabelList) {
+        this.mSetLabelList = mSetLabelList;
+    }
 }
