c752377d87d1ebaf7e318cd00ed397b80cd44ac1
kevuo <kevgiso@hotmail.com>
2016-04-05 10:12:23 -0600

    Add Simulation_Manager to start simulating instructions provided by the assembling stage.

--- mercury/simulator/Simulation_Manager.java
+++ mercury/simulator/Simulation_Manager.java
@@ -1,54 +1,98 @@
 package mercury.simulator;
 
-import jdk.nashorn.internal.runtime.Context;
 import mercury.ARMProgram;
 import mercury.ProgramStatement;
+import mercury.arm.instruction.Constants;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by Kevin on 03/04/2016.
  */
 public class Simulation_Manager {
-    private static final int DATAPROCESSING = 0;
-    private static final int MEMORY = 1;
-    private static final int BRANCH = 0;
 
 
     public Simulation_Manager() {
     }
 /*
     public void Simulate(ARMProgram mainProgram) {
-        ArrayList<ProgramStatement> instructionSet = new ArrayList<>();
-        //instructionSet = mainProgram.getStatementList();
+        List<ProgramStatement> instructionSet;
+        instructionSet = mainProgram.getStatementList();
         ProgramStatement currentStatement = new ProgramStatement();
-        String instructionCategory = currentStatement.getInstructionType();
-        for (currentStatement:
-             instructionSet) {
-            switch (instructionCategory) {
-                case "DATAPROCESSING":
-                    DataProcessingInstructionManager();
+        String regularMnemonic;
+        /*
+        for (int i = 0; i < instructionSet.size(); i++) {
+            currentStatement = instructionSet.get(i);
+            regularMnemonic = GetRegularMnemonic(currentStatement);
+            switch (regularMnemonic) {
+                case Constants.AND_MNEMONIC:
                     break;
                 case "MEMORY":
-                    MemoryInstructionManager();
                     break;
                 case "BRANCH":
-                    BranchInstructionManager();
                     break;
-                default:
-                    System.out.println("Invalid instruction");
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
+                    break;
+                case "BRANCH":
                     break;
 
             }
-        }*/
-/*
-    public int CategorizeProgramStatement(ProgramStatement instruction) {
-        String instructionType = instruction.getInstructionType;
-        if (instructionType.equals("")) {
 
         }
-
-
     }*/
+
+    private String GetRegularMnemonic(ProgramStatement instruction) {
+        String instrMnemonic = instruction.getMnemonic();
+        int instrMnemonicLength = instrMnemonic.length();
+        if (instruction.HasSetFlagsSufix() & !instruction.HasConditionalSufix()) {
+            instrMnemonic = instrMnemonic.substring(0, instrMnemonicLength - 1);
+        } else if (!instruction.HasSetFlagsSufix() & instruction.HasConditionalSufix()) {
+            instrMnemonic = instrMnemonic.substring(0, instrMnemonicLength - 2);
+        } else if (instruction.HasConditionalSufix() & instruction.HasSetFlagsSufix()) {
+            instrMnemonic = instrMnemonic.substring(0, instrMnemonicLength - 3);
+        }
+    return instrMnemonic;
+    }
 }
